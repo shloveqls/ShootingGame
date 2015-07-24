20a90e81db83c2dbb87c12ffe79fb1d9a2eb3292
shloveqls <shloveqls@gmail.com>
2015-07-24 19:58:42 +0900

    第03回　プレイヤーから弾を撃つ

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+
+		Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D> ();
+		rigidbody2D.velocity = gameObject.transform.up.normalized * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,17 @@ public class Player : MonoBehaviour {
 	// 移動スピード
 	public float speed = 5;
 
-	// Use this for initialization
-	void Start () {
-	
+	// PlayerBulletプレハブ
+	public GameObject bullet;
+
+	// Startメソッドをコルーチンとして呼び出す
+	IEnumerator Start () {
+		while (true) {
+			// 弾をプレイヤーと同じ位置/角度で作成
+			Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+			// 0.05秒待つ
+			yield return new WaitForSeconds(0.05f);
+		}
 	}
 	
 	// Update is called once per frame
