712bf86948b78c84fa8852129fd275c85f9ad3b2
shloveqls <shloveqls@gmail.com>
2015-07-24 16:50:46 +0900

    第02回　プレイヤーの移動

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	// 移動スピード
+	public float speed = 5;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,19 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		// 右・左
+		float x = Input.GetAxisRaw ("Horizontal");
+
+		// 上・下
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// 移動する向きを求める
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		// 移動する向きとスピードを代入する
+		Rigidbody2D rigidbody2D = gameObject.GetComponent<Rigidbody2D> ();
+		rigidbody2D.velocity = direction * speed;
+
 	}
+
 }
